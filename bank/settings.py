5a91a699d8ee6e3e47a1edd38cb65890b3c0fe6c
Marcelinus Apakhade <maarrxxy@gmail.com>
2024-10-06 10:17:14 +0100

    Update settings.py

--- bank/settings.py
+++ bank/settings.py
@@ -186,3 +186,10 @@ LOGGING = {
         },
     },
 }
+
+
+CELERY_BROKER_URL = 'redis://localhost:6379/0'  # Or your chosen broker
+CELERY_ACCEPT_CONTENT = ['json']
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
+
