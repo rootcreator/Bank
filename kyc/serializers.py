7062203745801b2efa8d20d7459ca4a2d1387009
Marcelinus Apakhade <maarrxxy@gmail.com>
2024-10-06 10:12:43 +0100

    Update serializers.py

--- kyc/serializers.py
+++ kyc/serializers.py
@@ -1,8 +1,13 @@
 from rest_framework import serializers
+from app.models import UserProfile
 from .models import KYCRequest
 
+class UserProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = UserProfile
+        fields = '__all__'  # Adjust based on what fields you want to expose
 
 class KYCRequestSerializer(serializers.ModelSerializer):
     class Meta:
         model = KYCRequest
-        fields = ['id_document', 'selfie', 'address_document']
+        fields = ['document_type', 'document_file', 'address_proof_file', 'selfie_file']
